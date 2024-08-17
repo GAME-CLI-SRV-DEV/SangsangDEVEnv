b8285737598481820f1e7cd2bf8c85fcc0a0a95f
Outfit8TSB <sunnybunnies09@naver.com>
2024-08-17 10:54:14 +0900

    Update 상상플러그인.bat

--- 상상플러그인.bat
+++ 상상플러그인.bat
@@ -9,9 +9,12 @@ echo 서버 실행에 사용되지 않습니다(즉 버킷이 아닙니다)! 상
 set 저장소=https://github.com/GAME-CLI-SRV-DEV/
 set 플러그인이름=FastCrystals-Sangsang
 set 실행버킷=https://github.com/GAME-CLI-SRV-DEV/Sangsang
-set 버킷폴더=Sangsang
+set 버킷폴더=SangsangPlugin
 git clone %실행버킷% %버킷폴더%
 cd %버킷폴더%
+del gradle.properties
+cd ..
+copy gradle.properties to %버킷폴더%
 git submodule add %저장소%%플러그인이름%
 git submodule init
 gradlew applypatches && gradlew build
