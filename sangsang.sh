838bcd6649c06b20215af3fc4ceecabd0443658d
Outfit8TSB <sunnybunnies09@naver.com>
2024-08-28 16:30:52 +0900

    Update sangsang.sh

--- sangsang.sh
+++ sangsang.sh
@@ -4,12 +4,18 @@ echo "상상서버 - 상상 서버 버킷 빌드 도구"
 echo "서버 버킷을 빌드합니다."
 echo "이 빌드에서 나온 결과물인 Sangsang-Paperclip-1.20.6-R0.1-SNAPSHOT-reobf.jar은 사용이 가능합니다."
 
-실행버킷="https://github.com/GAME-CLI-SRV-DEV/Sangsang"
-버킷폴더="Sangsang"
+# Set variables
+repo_url="https://github.com/GAME-CLI-SRV-DEV/Sangsang"
+folder_name="Sangsang"
 
-echo "실행버킷: $실행버킷"
-echo "버킷폴더: $버킷폴더"
+# Debugging: Print variables to check if they are set correctly
+echo "repo_url: $repo_url"
+echo "folder_name: $folder_name"
 
-git clone $실행버킷 $버킷폴더
-cd $버킷폴더
+
+echo "실행버킷: $repo_url"
+echo "버킷폴더: $folder_name"
+
+git clone "$repo_url" "$folder_name"
+cd "$folder_name"
 ./gradlew applypatches && ./gradlew createreobfPaperclipjar
